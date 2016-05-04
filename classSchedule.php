62f7cba1009401b3295d7cff381b0d2a45b40dfa
WSiaB <johnl@kirkersoft.com>
2016-05-04 12:48:48 -0700

    Added Personal Protection class info Updated class resumption to Fall 2016

--- classSchedule.php
+++ classSchedule.php
@@ -41,7 +41,8 @@
 </head>
   <body topmargin="0" marginheight="0" vlink="white">
     <center class="gwd-center-mlcc active-element-outline">
-      <table class="gwd-table-j4a0" background="media/background.png" border="0"
+      <table class="gwd-table-j4a0" background="media/background.png" border="0"
+
         height="100%" width="1000">
         <tbody>
           <tr>
@@ -68,8 +69,10 @@
               </center>
               <div align="left">.
                 <!-- Red Cross First Aid CPR AED
-                <p><a id="RedCrossFirstAidCPR" name="RedCrossFirstAidCPR"></a> <strong><a
-                      href="classDescriptions.html#RedCrossFirstAidCPR"><font color="yellow"
+                <p><a id="RedCrossFirstAidCPR" name="RedCrossFirstAidCPR"></a> <strong><a
+
+                      href="classDescriptions.html#RedCrossFirstAidCPR"><font color="yellow"
+
                         size="4"> Red Cross Adult First Aid/CPR/AED</font></a></strong>
                 </p>
                 <p>Date/Time: March 14th at 9:00AM <br>
@@ -82,22 +85,27 @@
                 <p>.</p>
 				
                 <!-- NRA Basic Pistol
-                <p><a id="NRABasicPistol" name="NRABasicPistol"></a> <strong><a
-                      href="classDescriptions.html#NRABasicPistol"><font color="yellow"
+                <p><a id="NRABasicPistol" name="NRABasicPistol"></a> <strong><a
+
+                      href="classDescriptions.html#NRABasicPistol"><font color="yellow"
+
                         size="+1">NRA Basic Pistol</font></a></strong></p>
                 <p>Date/Time: March 28th at 9:00AM<br>
                   Location: Pleasant Hill, CA<br>
                   Course Length: 8 hours<br>
                   Course Cost: <strike>$150</strike> <font color="red">Early
                     Bird Offer</font>: Only $100 (if you sign up by 03/14/2015)</p>
-                <p><a target="_blank" href="https://www.eventbrite.com/e/nra-basic-pistol-registration-15763863139"><span
+                <p><a target="_blank" href="https://www.eventbrite.com/e/nra-basic-pistol-registration-15763863139"><span
+
                       style="font-weight: bold;">SIGN UP NOW</span></a> (limited
                   class size)</p>
                 <p>.</p>
 				
                 <!-- Women On Target
-                <p> <a id="WomenonTarget" name="WomenonTarget"></a> <strong><a
-                      href="classDescriptions.html#WomenonTarget"><font color="yellow"
+                <p> <a id="WomenonTarget" name="WomenonTarget"></a> <strong><a
+
+                      href="classDescriptions.html#WomenonTarget"><font color="yellow"
+
                         size="+1">Women On Target (Small Caliber)</font></a></strong>
                   sponsored by Tri Valley NRA Members Council</p>
                 <p>Date/Time: April 4th at 9:00AM<br>
@@ -116,7 +124,7 @@
                 <!-- SABRE Civilian Safety Awareness Program (CSAP)
                 <p><a id="SABREcsap" name="SABREcsap"></a><strong><a href="classDescriptions.html#SABREcsap"><font                        color="yellow" size="+1">SABRE Civilian Safety Awareness                        Program</font></a></strong></p>                Date/Time: TBD<br>                Location: TBD<br>                Course length: 3 hours<br>                Course cost: $65                <p><a href="mailto:johnl@diablofirearms.com?subject=06Sep2014_SABRE_CSAP_Class&amp;body=Name:%3Cbr%3EPhone:%3Cbr%3E">SIGN                    UP NOW</a></p>                <p>.</p>
 				-->
-				<h3>We will be resuming classes Summer 2016</h3>
+				<h3>We will be resuming classes Fall 2016</h3>
 				Thank you for your patience.<br>
 				<br><br><br><br><br>
 				<br><br><br><br><br>
@@ -124,14 +132,17 @@
               <h2 align="center"><em><a href="index.html">Home</a> - <a href="classDescriptions.html">Class
                     Descriptions</a> - Class Schedule - <a href="contactUs.html">Contact
                     Us</a></em></h2>
-              <div align="center"> ©2015 <a href="http://www.diablofirearmtraining.com/contactUs.html"
-                  target="_top">Diablo Safety Training</a> </div>
+              <div align="center"> ©2016 <a href="http://www.diablofirearmtraining.com/contactUs.html"
+
+                  target="_top">Diablo Firearms Training</a> </div>
             </td>
           </tr>
         </tbody>
       </table>
-      <!-- Facebook Badge START --> <a href="https://www.facebook.com/DiabloFirearms"
-        target="_TOP" title="Diablo Firearms Training"> <img src="https://badge.facebook.com/badge/572707706096242.3223.1308922374.png"
+      <!-- Facebook Badge START --> <a href="https://www.facebook.com/DiabloFirearms"
+
+        target="_TOP" title="Diablo Firearms Training"> <img src="https://badge.facebook.com/badge/572707706096242.3223.1308922374.png"
+
           style="border: 0px none ; position: absolute; float: right; right: 0px; bottom: 0px; z-index: 2;">
       </a>
       <!-- Facebook Badge END --> </center>
