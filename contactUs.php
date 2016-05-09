4edd1e4d35b386771ee55f8433c58f1005fdbce3
WSiaB <johnl@kirkersoft.com>
2016-05-09 12:19:28 -0700

    Replaced eddie eagle picture and removed background added glock armorer badge

--- contactUs.php
+++ contactUs.php
@@ -2,7 +2,7 @@
 	include_once('header.php');
 ?>
                 <u><font color="red" size="5"><strong>Contact Us</strong></font></u>
-              </center><h2> (925) 459-5995<br>
+              <h2> (925) 459-5995<br>
                   <a href="mailto:contactus@diablofirearms.com?subject=Contact_Us">contactUs@DiabloFirearms.com</a></h2>
                 &nbsp; <a href="http://www.usacarry.com/directory/diablo-firearms-training-4806.html"
 
@@ -22,28 +22,29 @@
                 <table style="width: 540px; height: 220px;" border="0">
                   <tbody>
                     <tr>
-                      <td valign="center" width="33%">
-						<a class="widget ng-scope" href="https://www.thumbtack.com/ca/concord/martial-arts-training/firearms-defense-training" target="_blank"> <img style="height: 128px;width: 128px;" src="https://static.thumbtackstatic.com/media/pages/profile/standard-widgets/pro-svg/white/2014.svg"></a><script type='text/javascript' src='https://www.thumbtack.com/profile/widgets/scripts/?service_id=Bykn8wYAnboBqA&widget_id=profile'></script>
+                      <td valign="center" width="33%" align=center>
+						<a href="https://us.glock.com/" target="_blank"> <img src="./media/glock.png" width=135 height=135 border=0> </a>
 					  </td>
-                      <td style="text-align: right;" valign="center"><a href="http://www.usacarry.com/directory/diablo-firearms-training-4806.html"
+                      <td valign="center" width="34%" align=center><a href="http://www.usacarry.com/directory/diablo-firearms-training-4806.html"
 
                           target="_blank"> <img src="http://www.usacarry.com/images/usa-carry-registered-instructor.gif"
 
                             alt="USA Carry Registered Instructor" border="0" height="125"
 
                             width="100"> </a></td>
+                      <td valign="center" width="33%" align=center><a href="http://eddieeagle.nra.org/information-for-parents.aspx"
+                          target="_blank"> <img src="./media/eddieEagle.png" border=0> </a></td>
                     </tr>
                     <tr>
-                      <td><a href="http://eddieeagle.nra.org/information-for-parents.aspx"
-                          target="_blank"> <img src="http://www.robotronics.com/crimeprevention/logos/eddielogo2.jpg"
-                            border="0" width=175 height=176></a></td>
-                      <td><br>
+                      <td valign="center" align=center><br>
                       </td>
-                      <td style="text-align: right;"><a href="http://www.projectchildsafe.org/"
+                      <td valign="center" align=center><a href="http://www.projectchildsafe.org/"
 
                           target="_blank"><img src="http://www.projectchildsafe.org/sites/default/files/logo_2.png"
 
                             border="0" height="68" width="104"></a></td>
+					  <td valign="center" align=center><br>
+					  </td>
                     </tr>
                   </tbody>
                 </table>
